d6311310be2305e536945c088ab195f3b7cfdd95
Max Alva <alva85@gmail.com>
2019-04-21 22:46:43 -0500

    Construyendo nuestra base de datos con Migraciones

---
+++ database/migrations/2019_04_22_032239_create_courses_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use App\Course;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('description');
+            $table->string('slug');
+            $table->string('picture')->nullable();
+            $table->enum('status', [Course::PUBLISHED, Course::PENDING, Course::REJECTED])
+                ->default(Course::PENDING);
+            $table->boolean('previus_approved')->default(false);
+            $table->boolean('previus_rejected')->default(false);
+
+            $table->unsignedInteger('teacher_id');
+            $table->foreign('teacher_id')->references('id')->on('teachers');
+
+            $table->unsignedInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories');
+
+            $table->unsignedInteger('level_id');
+            $table->foreign('level_id')->references('id')->on('levels');
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
