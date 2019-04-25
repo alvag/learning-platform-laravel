22f79777e06998a990c38d40155eca7ac13c0d53
Max Alva <alva85@gmail.com>
2019-04-24 22:15:57 -0500

    Middleware para configurar el idioma de nuestra aplicación en cada petición

---
+++ app/Http/Middleware/Language.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App;
+use Carbon\Carbon;
+use Closure;
+use Illuminate\Http\Request;
+
+class Language
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (session('applocale')) {
+            $configLanguage = config('languages')[session('applocale')];
+            setlocale(LC_TIME, $configLanguage[1] . '.utf8');
+            Carbon::setLocale(session('applocale'));
+            App::setLocale(session('applocale'));
+        } else {
+            session()->put('applocale', config('app.fallback_locale'));
+            setlocale(LC_TIME, 'es_ES.utf8');
+            Carbon::setLocale(session('applocale'));
+            App::setLocale(config('app.fallback_locale'));
+
+        }
+        return $next($request);
+    }
+}
